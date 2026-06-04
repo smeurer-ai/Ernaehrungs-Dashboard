90213589a1b28cef6ec7d63fbf224af820705119
Stephanie Meurer <stephanie.meurer@gmx.de>
2026-06-04 13:47:08 +0200

    feat: RECIPE_MEAL_SLOTS und INGREDIENT_UNIT_SUGGESTIONS Konstanten

---
+++ js/data/mealSlots.js
@@ -0,0 +1,15 @@
+// js/data/mealSlots.js
+export const RECIPE_MEAL_SLOTS = [
+  'Frühstück',
+  'Mittagessen',
+  'Abendessen',
+  'Pre-Workout',
+  'Post-Workout',
+  'Snack',
+  'Casein',
+];
+
+export const INGREDIENT_UNIT_SUGGESTIONS = [
+  'g', 'ml', 'Stk', 'EL', 'TL',
+  'Packung', 'Portion', 'Scheibe', 'Dose',
+];
